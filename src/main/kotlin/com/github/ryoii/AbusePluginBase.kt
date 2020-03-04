077edcdc6c2fbdb736d0ef0bbe4ec2321fa1b8b7
ryoii <ryoii@foxmail.com>
2020-03-04 16:40:55 +0800

    release 1.1.0

--- src/main/kotlin/com/github/ryoii/AbusePluginBase.kt
+++ src/main/kotlin/com/github/ryoii/AbusePluginBase.kt
@@ -15,27 +15,49 @@ class AbusePluginBase : PluginBase() {
         const val api = "https://nmsl.shadiao.app/api.php?lang=zh_cn&level="
     }
 
-    val setting by lazy{
+    val setting by lazy {
         this.loadConfig("setting.yml")
     }
 
+    val owner by setting.withDefault { 0L }
     val level by setting.withDefault { "min" }
+    private val whiteList by setting.withDefault { mutableListOf<Long>() }
+    val whiteSet = whiteList.toHashSet()
+    val keyword by setting.withDefault { listOf("骂") }
 
     override fun onEnable() {
         super.onEnable()
 
         subscribeFriendMessages {
-            contains("骂") {
+            content({ keyword.any { key -> it.contains(key, ignoreCase = true) } }) {
                 reply(PlainText(Http.get(api + level)))
             }
         }
 
         subscribeGroupMessages {
-            contains("骂") {
+            content({ (group.id in whiteSet) and keyword.any { key -> it.contains(key, ignoreCase = true) } }) {
                 if (message.any { it is At }) {
                     reply(message[At] + PlainText(Http.get(api + level)))
                 } else {
                     reply(sender.at() + PlainText(Http.get(api + level)))
+                }
+            }
+        }
+
+
+        if (owner != 0L) {
+            subscribeGroupMessages {
+                (sentBy(owner) and startsWith("口吐芬芳")) {
+                    when {
+                        it.endsWith("开") -> {
+                            whiteSet.add(group.id)
+                            reply("本群开")
+                        }
+                        it.endsWith("关") -> {
+                            whiteSet.remove(group.id)
+                            reply("本群关")
+                        }
+                    }
                 }
             }
         }
